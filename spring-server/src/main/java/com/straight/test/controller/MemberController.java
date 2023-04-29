b9a90140e6ad70457337d9ae5262f134ee255ea8
mkyoung24 <gulpa009@gmail.com>
2023-04-29 16:37:25 +0900

    [Create]Team-CRUD

--- spring-server/src/main/java/com/straight/test/controller/MemberController.java
+++ spring-server/src/main/java/com/straight/test/controller/MemberController.java
@@ -2,8 +2,11 @@ package com.straight.test.controller;
 
 import com.straight.test.domain.Member;
 import com.straight.test.domain.dto.MemberDTO;
+import com.straight.test.domain.dto.PageRequestDTO;
 import com.straight.test.service.MemberService;
+import com.straight.test.service.TeamService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
@@ -16,9 +19,12 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/test")
 @RequiredArgsConstructor
+@Log4j2
 public class MemberController {
 
     private final MemberService memberService;
+
+    private final TeamService teamService;
 
     // 회원가입 페이지로 navi
     @GetMapping("/joinForm")
@@ -32,13 +38,13 @@ public class MemberController {
 
 
     // 회원 가입
-    @PostMapping("/member")
+    @PostMapping("/member")     //회원가입
     public String joinMember(MemberDTO member) {
         Member result = memberService.joinMember(member);
         return "redirect:/test/loginForm";
     }
 
-    @PostMapping("/member/login")
+    @PostMapping("/member/login")   //로그인
     public String login(String user_id, String user_pw, HttpServletRequest request) {
 
         boolean login = memberService.loginMember(user_id, user_pw);
@@ -47,7 +53,7 @@ public class MemberController {
             session.setAttribute("loginState", "login");
             session.setAttribute("loginId", user_id);
             session.setAttribute("loginPw", user_pw);
-            return "../static/index";
+            return "redirect:/team/list";
         }
         else {
             session.setAttribute("loinState", "logout");
@@ -55,13 +61,13 @@ public class MemberController {
         }
     }
 
-    @GetMapping("/logout")
+    @GetMapping("/logout")      //로그아웃
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/test/loginForm";
     }
 
-    @GetMapping("/member")
+    @GetMapping("/member")      //회원 정보 불러오기
     public String updateMember(String user_id, Model model) {
 
         MemberDTO dto = memberService.read(user_id);
@@ -69,13 +75,13 @@ public class MemberController {
         return "member";
     }
 
-    @PostMapping("/update")
+    @PostMapping("/update")     //회원 정보 수정
     public String updateMember(MemberDTO member) {
         memberService.modify(member);
-        return "../static/index";
+        return "redirect:/team/list";
     }
 
-    @GetMapping("/delete")
+    @GetMapping("/delete")      //회원탙퇴
     public String deleteMember(@RequestParam("id") Long id) {
         memberService.remove(id);
         return "loginForm";
