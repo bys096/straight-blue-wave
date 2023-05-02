fffa22080fb2ade4f235dc56a96ac56498095854
Sungbin00 <ddusin0614@naver.com>
2023-05-02 17:29:29 +0900

    [update] app.js 수정

--- react-server/src/App.js
+++ react-server/src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
 import Footer from "./components/views/Footer";
 import Header from "./components/views/Header";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/SignUp";
 import MainPage from "./pages/MainPage";
 import Update from "./pages/UpdatePage";
 import Sidebar from "./components/views/Sidebar";
-import DefaultPage from "./pages/DefaultPage"
+import DefaultPage from "./pages/DefaultPage";
 import SignUp from "./pages/SignUp";
 import LoggedIn from "./pages/LoggedIn";
 
 function App() {
-
 	return (
 		<div className="App">
 			<Routes>
@@ -19,9 +18,8 @@ function App() {
 				<Route path="/SignUp" exact={true} Component={SignUp} />
 				<Route path="/LoggedIn" exact={true} Component={LoggedIn} />
 			</Routes>
-
 		</div>
-	)
+	);
 }
 
 export default App;
