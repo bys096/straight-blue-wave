90efd2038e8f7a7955f5814b75739c652cedf3bb
monya-9 <64175828+monya-9@users.noreply.github.com>
2023-05-06 19:19:35 +0900

    api추가 webMvcConfig 수정

--- spring-server/src/main/java/com/straight/test/controller/MemberController.java
+++ spring-server/src/main/java/com/straight/test/controller/MemberController.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 @Log4j2
 public class MemberController {
