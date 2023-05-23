e5c2242900018c7f8a2b6f08d717f38d8b9b3dfe
mkyoung24 <gulpa009@gmail.com>
2023-05-23 10:11:58 +0900

    [add]logout

--- spring-server/bluewave/src/main/java/com/straight/bluewave/application/controller/AuthController.java
+++ spring-server/bluewave/src/main/java/com/straight/bluewave/application/controller/AuthController.java
@@ -32,7 +32,7 @@ public class AuthController {
 
         Cookie cookie = new Cookie("token", tokenDTO.getRefreshToken());        //RefreshToken을 쿠키에 저장
         cookie.setMaxAge(3600);     //초 단위 시간
-        cookie.setPath("/");        //쿠기 경로 적용하기
+        cookie.setPath("/api");        //쿠기 경로 적용하기
         response.addCookie(cookie);
 
         response.setHeader("Authorization", "Bearer " + tokenDTO.getAccessToken());     //AccessToken을 헤더에 담아 응답
@@ -41,12 +41,25 @@ public class AuthController {
         //return ResponseEntity.ok(authService.login(memberRequestDto));
     }
 
+    @PostMapping("/logout")     //로그아웃
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);        //쿠키 만료시간을 0으로 설정
+        cookie.setPath("/api");
+        response.addCookie(cookie);
+
+        response.setHeader("Authorization", "");
+
+        return ResponseEntity.ok("로그아웃 성공");
+    }
+
+
     @PostMapping("/reissue")        //재발급
     public ResponseEntity<TokenDTO> reissue(@RequestBody TokenRequestDTO tokenRequestDto, HttpServletResponse response) {
         TokenDTO tokenDTO = authService.reissue(tokenRequestDto);
 
         Cookie cookie = new Cookie("token", tokenDTO.getRefreshToken());
-        cookie.setPath("/");
+        cookie.setPath("/api");
         cookie.setMaxAge(3600);
 
         response.setHeader("Authorization", "Bearer " + tokenDTO.getAccessToken());
