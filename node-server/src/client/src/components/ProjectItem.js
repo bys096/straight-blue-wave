d342b0ed336b37497c0ce7c0cd252426324a207c
Sungbin00 <ddusin0614@naver.com>
2023-06-15 20:06:47 +0900

    '친구추가중복해결,팀원추가중복해결,관리자만프로젝트생성,회원추가,팀삭제,프로젝트삭제가능하게구현'

--- node-server/src/client/src/components/ProjectItem.js
+++ node-server/src/client/src/components/ProjectItem.js
@@ -6,45 +6,46 @@ import { useDispatch } from "react-redux";
 import { projectConnect } from "../actions/project";
 
 const StyledCard = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  cursor: pointer;
+  width: 130px; // 원하는 너비 지정
+  height: 180px; // 원하는 높이 지정
+  border-radius: 10px; // 카드 모서리 둥글게 처리
+  text-decoration: none;
+  user-select: none;
 
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-	cursor: pointer;
-	width: 130px; // 원하는 너비 지정
-	height: 180px; // 원하는 높이 지정
-	border-radius: 10px; // 카드 모서리 둥글게 처리
-	text-decoration: none;
-	user-select: none;
-	
-	&:hover {
-		transform: scale(1.05);
-		transition: all 0.2s ease-in-out;
-	}
+  &:hover {
+    transform: scale(1.05);
+    transition: all 0.2s ease-in-out;
+  }
 `;
 
 const ProjectItem = ({ project = [] }) => {
-	const dispatch = useDispatch();
-	return (
-		<>
-			<Link
-				to={`/project/${project.prjId}`}
-				className="text-decoration-none"
-				onClick={() => {
-        sessionStorage.setItem("prjid", project.prjId);
-        sessionStorage.setItem("prjroom", project.prjRoom);
-				dispatch(projectConnect(project));}}
-			>
-				<StyledCard>
-					<Card.Body>
-						<Card.Title>{project.prjName}</Card.Title>
-					</Card.Body>
-				</StyledCard>
-			</Link>
-		</>
-	);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <Link
+        to={`/project/${project.prjId}`}
+        className="text-decoration-none"
+        onClick={() => {
+          sessionStorage.setItem("prjid", project.prjId);
+          sessionStorage.setItem("prjroom", project.prjRoom);
+          sessionStorage.setItem("prjname", project.prjName);
+          dispatch(projectConnect(project));
+        }}
+      >
+        <StyledCard>
+          <Card.Body>
+            <Card.Title>{project.prjName}</Card.Title>
+          </Card.Body>
+        </StyledCard>
+      </Link>
+    </>
+  );
 };
 
 export default ProjectItem;
