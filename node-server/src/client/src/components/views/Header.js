a7a2c53cc5e0695fd1c839e38f4f4abcd8f8e291
Sungbin00 <ddusin0614@naver.com>
2023-06-25 01:35:04 +0900

    '[update]알림기능추가'

--- node-server/src/client/src/components/views/Header.js
+++ node-server/src/client/src/components/views/Header.js
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import {
-	Nav,
-	Navbar,
-	Offcanvas,
-	Button,
-	OverlayTrigger,
-	Accordion,
+  Nav,
+  Navbar,
+  Offcanvas,
+  Button,
+  OverlayTrigger,
+  Accordion,
+  Modal,
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AiOutlineBell } from "react-icons/ai";
@@ -25,258 +26,309 @@ import { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../actions/auth";
 import l1 from "../../assets/1_1.png";
+import axios from "axios";
 
 function Header() {
-	const ref = useRef(null);
-	const dispatch = useDispatch();
-	const [showHelp, setShowHelp] = useState(false);
-	const [showNotify, setShowNotify] = useState(false);
+  const ref = useRef(null);
+  const dispatch = useDispatch();
+  const [showHelp, setShowHelp] = useState(false);
+  const [showNotify, setShowNotify] = useState(false);
 
-	const [helpContent, setHelpContent] = useState("");
-	const [previousContent, setPreviousContent] = useState("");
+  const [helpContent, setHelpContent] = useState("");
+  const [previousContent, setPreviousContent] = useState("");
 
-	const [target, setTarget] = useState(null);
+  const [target, setTarget] = useState(null);
 
-	const handleCloseHelp = () => {
-		setShowHelp(false);
-		setPreviousContent("");
-	};
+  const [notifications, setNotifications] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
-	const handleShowHelp = (content) => {
-		setHelpContent(content);
-		setPreviousContent(helpContent);
-		setShowHelp(true);
-	};
+  const handleCloseHelp = () => {
+    setShowHelp(false);
+    setPreviousContent("");
+  };
 
-	const handleGoBack = () => {
-		setHelpContent("guide");
-		setPreviousContent("");
-	};
+  const handleShowHelp = (content) => {
+    setHelpContent(content);
+    setPreviousContent(helpContent);
+    setShowHelp(true);
+  };
 
-	const handleShowNotify = (e) => {
-		setShowNotify(!showNotify);
-		setTarget(e.target);
-	};
+  const handleGoBack = () => {
+    setHelpContent("guide");
+    setPreviousContent("");
+  };
 
-	return (
-		<>
-			<Navbar collapseOnSelect expand="lg" bg="white" variant="white" sticky="top" className="shadow-sm">
-				<Container style={{ display: "flex", justifyContent: "space-between" }}>
-					<Navbar.Brand>
-						<Link to="/LoggedIn">
-							<img
-								src={l1}
-								width="60"
-								height="40"
-								className="d-inline-block align-top"
-								alt="React Bootstrap logo"
-							/>
-						</Link>
-					</Navbar.Brand>
-					<Navbar.Brand style={{ color: "#0085AD", margin: "0 20px 0 -10px" }}>
-						<Link to="/LoggedIn" style={{ textDecoration: "none", color: "#0085AD" }}>
-							Blue
-							<span style={{ color: "black" }}>Wave</span>
-						</Link>
-					</Navbar.Brand>
-					<Navbar.Collapse id="responsive-navbar-nav">
-						<Nav style={{ marginLeft: "auto" }}>
-							<Nav.Link
-								onClick={() => handleShowHelp("guide")}
-								style={{ cursor: "pointer" }}
-							>
-								도움말
-							</Nav.Link>
-							<Nav.Link onClick={handleShowNotify}>
-								<AiOutlineBell size={"1.5rem"} />
-							</Nav.Link>
-							<Nav.Link>
-								<Link to="/setting" style={{ textDecoration: "none", color: "#0085AD" }}>
-									설정
-								</Link>
-							</Nav.Link>
-							<Nav.Link>
-								<Link
-									to="/"
-									style={{ textDecoration: "none", color: "#0085AD" }}
-									onClick={() => {
-										dispatch(logout());
-										sessionStorage.clear();
-									}}
-								>
-									로그아웃
-								</Link>
-							</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-			<Offcanvas
-				show={showHelp}
-				onHide={handleCloseHelp}
-				placement="end"
-				scroll={true}
-				backdrop={false}
-			>
-				<Offcanvas.Header closeButton>
-					<Offcanvas.Title>도움말</Offcanvas.Title>
-				</Offcanvas.Header>
-				<Offcanvas.Body>
-					<Accordion>
-						{helpContent === "guide" && (
-							<>
-								<p>
-									블루웨이브에선 여러분들의 회의를 돕기 위한 회의 가이드라인을
-									제공합니다. 회의 진행을 하다 막히신다면 저희 가이드라인을 참조해보시길
-									바랍니다.
-								</p>
-								<p>아래에서 원하는 섹션을 선택하세요:</p>
+  const fetchNotifications = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8002/api/notification/list/${sessionStorage.getItem(
+          "memid"
+        )}`
+      );
+      setNotifications(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-								<ul>
-									<li>
-										<Button
-											variant="primary"
-											onClick={() => handleShowHelp("idea-generation")}
-										>
-											아이디어 창출 기법
-										</Button>
-									</li>
-									<li>
-										<Button
-											variant="primary"
-											onClick={() => handleShowHelp("meeting-process")}
-										>
-											회의 진행 방식
-										</Button>
-									</li>
-									<li>
-										<Button
-											className="primary"
-											onClick={() => handleShowHelp("meeting-mindset")}
-										>
-											회의 진행 마음가짐
-										</Button>
-									</li>
-								</ul>
-							</>
-						)}
-						{helpContent === "idea-generation" && (
-							<>
-								<h4>아이디어 창출 기법</h4>
-								<ul>
-									<li onClick={() => handleShowHelp("brainstorming")}>브레인스토밍</li>
-									<li onClick={() => handleShowHelp("brainwriting")}>브레인라이팅</li>
-									<li onClick={() => handleShowHelp("kjmapping")}>KJ법</li>
-									<li onClick={() => handleShowHelp("mindmap")}>마인드맵</li>
-									<li onClick={() => handleShowHelp("synectics")}>시네틱스</li>
-									<li onClick={() => handleShowHelp("scamper")}>스캠퍼</li>
-									<li onClick={() => handleShowHelp("sixthinkinghats")}>여섯색깔모자</li>
-								</ul>
-								<Button variant="primary" onClick={handleGoBack}>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-						{helpContent === "meeting-process" && (
-							<>
-								<Meetingprocess></Meetingprocess>
-								<Button variant="primary" onClick={handleGoBack}>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-						{helpContent === "meeting-mindset" && (
-							<>
-								<Meetingmindset></Meetingmindset>
-								<Button variant="primary" onClick={handleGoBack}>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-						{helpContent === "brainstorming" && (
-							<>
-								<Brainstorming></Brainstorming>
-								<Button
-									variant="primary"
-									onClick={() => handleShowHelp("idea-generation")}
-								>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-						{helpContent === "brainwriting" && (
-							<>
-								<Brainwriting></Brainwriting>
-								<Button
-									variant="primary"
-									onClick={() => handleShowHelp("idea-generation")}
-								>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-						{helpContent === "kjmapping" && (
-							<>
-								<KJmapping></KJmapping>
-								<Button
-									variant="primary"
-									onClick={() => handleShowHelp("idea-generation")}
-								>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-						{helpContent === "mindmap" && (
-							<>
-								<Mindmap></Mindmap>
-								<Button
-									variant="primary"
-									onClick={() => handleShowHelp("idea-generation")}
-								>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-						{helpContent === "scamper" && (
-							<>
-								<SCAMPER></SCAMPER>
-								<Button
-									variant="primary"
-									onClick={() => handleShowHelp("idea-generation")}
-								>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-						{helpContent === "sixthinkinghats" && (
-							<>
-								<Sixthinkinghats></Sixthinkinghats>
-								<Button
-									variant="primary"
-									onClick={() => handleShowHelp("idea-generation")}
-								>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-						{helpContent === "synectics" && (
-							<>
-								<Synectics></Synectics>
-								<Button
-									variant="primary"
-									onClick={() => handleShowHelp("idea-generation")}
-								>
-									뒤로 가기
-								</Button>
-							</>
-						)}
-					</Accordion>
-				</Offcanvas.Body>
-			</Offcanvas>
+  const handleToggleModal = () => {
+    setShowModal(!showModal);
+  };
 
-			{showNotify && <PopOver ref={ref} target={target} show={showNotify} />}
-		</>
-	);
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
+
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="white"
+        variant="white"
+        sticky="top"
+        className="shadow-sm"
+      >
+        <Container style={{ display: "flex", justifyContent: "space-between" }}>
+          <Navbar.Brand>
+            <Link to="/LoggedIn">
+              <img
+                src={l1}
+                width="60"
+                height="40"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Brand style={{ color: "#0085AD", margin: "0 20px 0 -10px" }}>
+            <Link
+              to="/LoggedIn"
+              style={{ textDecoration: "none", color: "#0085AD" }}
+            >
+              Blue
+              <span style={{ color: "black" }}>Wave</span>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav style={{ marginLeft: "auto" }}>
+              <Nav.Link
+                onClick={() => handleShowHelp("guide")}
+                style={{ cursor: "pointer" }}
+              >
+                도움말
+              </Nav.Link>
+              <Nav.Link onClick={handleToggleModal}>
+                <AiOutlineBell size={"1.5rem"} />
+              </Nav.Link>
+              <Nav.Link>
+                <Link
+                  to="/setting"
+                  style={{ textDecoration: "none", color: "#0085AD" }}
+                >
+                  설정
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link
+                  to="/"
+                  style={{ textDecoration: "none", color: "#0085AD" }}
+                  onClick={() => {
+                    dispatch(logout());
+                    sessionStorage.clear();
+                  }}
+                >
+                  로그아웃
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Offcanvas
+        show={showHelp}
+        onHide={handleCloseHelp}
+        placement="end"
+        scroll={true}
+        backdrop={false}
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>도움말</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Accordion>
+            {helpContent === "guide" && (
+              <>
+                <p>
+                  블루웨이브에선 여러분들의 회의를 돕기 위한 회의 가이드라인을
+                  제공합니다. 회의 진행을 하다 막히신다면 저희 가이드라인을
+                  참조해보시길 바랍니다.
+                </p>
+                <p>아래에서 원하는 섹션을 선택하세요:</p>
+
+                <ul>
+                  <li>
+                    <Button
+                      variant="primary"
+                      onClick={() => handleShowHelp("idea-generation")}
+                    >
+                      아이디어 창출 기법
+                    </Button>
+                  </li>
+                  <li>
+                    <Button
+                      variant="primary"
+                      onClick={() => handleShowHelp("meeting-process")}
+                    >
+                      회의 진행 방식
+                    </Button>
+                  </li>
+                  <li>
+                    <Button
+                      className="primary"
+                      onClick={() => handleShowHelp("meeting-mindset")}
+                    >
+                      회의 진행 마음가짐
+                    </Button>
+                  </li>
+                </ul>
+              </>
+            )}
+            {helpContent === "idea-generation" && (
+              <>
+                <h4>아이디어 창출 기법</h4>
+                <ul>
+                  <li onClick={() => handleShowHelp("brainstorming")}>
+                    브레인스토밍
+                  </li>
+                  <li onClick={() => handleShowHelp("brainwriting")}>
+                    브레인라이팅
+                  </li>
+                  <li onClick={() => handleShowHelp("kjmapping")}>KJ법</li>
+                  <li onClick={() => handleShowHelp("mindmap")}>마인드맵</li>
+                  <li onClick={() => handleShowHelp("synectics")}>시네틱스</li>
+                  <li onClick={() => handleShowHelp("scamper")}>스캠퍼</li>
+                  <li onClick={() => handleShowHelp("sixthinkinghats")}>
+                    여섯색깔모자
+                  </li>
+                </ul>
+                <Button variant="primary" onClick={handleGoBack}>
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+            {helpContent === "meeting-process" && (
+              <>
+                <Meetingprocess></Meetingprocess>
+                <Button variant="primary" onClick={handleGoBack}>
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+            {helpContent === "meeting-mindset" && (
+              <>
+                <Meetingmindset></Meetingmindset>
+                <Button variant="primary" onClick={handleGoBack}>
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+            {helpContent === "brainstorming" && (
+              <>
+                <Brainstorming></Brainstorming>
+                <Button
+                  variant="primary"
+                  onClick={() => handleShowHelp("idea-generation")}
+                >
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+            {helpContent === "brainwriting" && (
+              <>
+                <Brainwriting></Brainwriting>
+                <Button
+                  variant="primary"
+                  onClick={() => handleShowHelp("idea-generation")}
+                >
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+            {helpContent === "kjmapping" && (
+              <>
+                <KJmapping></KJmapping>
+                <Button
+                  variant="primary"
+                  onClick={() => handleShowHelp("idea-generation")}
+                >
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+            {helpContent === "mindmap" && (
+              <>
+                <Mindmap></Mindmap>
+                <Button
+                  variant="primary"
+                  onClick={() => handleShowHelp("idea-generation")}
+                >
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+            {helpContent === "scamper" && (
+              <>
+                <SCAMPER></SCAMPER>
+                <Button
+                  variant="primary"
+                  onClick={() => handleShowHelp("idea-generation")}
+                >
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+            {helpContent === "sixthinkinghats" && (
+              <>
+                <Sixthinkinghats></Sixthinkinghats>
+                <Button
+                  variant="primary"
+                  onClick={() => handleShowHelp("idea-generation")}
+                >
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+            {helpContent === "synectics" && (
+              <>
+                <Synectics></Synectics>
+                <Button
+                  variant="primary"
+                  onClick={() => handleShowHelp("idea-generation")}
+                >
+                  뒤로 가기
+                </Button>
+              </>
+            )}
+          </Accordion>
+        </Offcanvas.Body>
+      </Offcanvas>
+      <Modal show={showModal} onHide={handleToggleModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>알림</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {notifications.map((notification) => (
+            <div key={notification.mn_id}>
+              {" "}
+              <h6>새로운 일정이 등록되었습니다. 프로젝트를 확인해 주세요</h6>
+              <hr></hr>
+            </div>
+          ))}
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
 
 export default Header;
